51cd868bac09ed52104abafd9b59d2b63f700f69
Michal Mojek <m.mojek@gmail.com>
2019-10-04 13:40:34 +0200

    Add celery to the project

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'core.CustomUser'
+
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
